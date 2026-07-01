5fef7d5a00431c1035d18df067ef193c0fa3b8a8
PeryPiseth303 <setholo303@gmail.com>
2026-07-01 19:07:38 +0700

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Orders from './Page/Orders'
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://food-del-backend-dztx.onrender.com"
 
   return (
     <div >
